cb144c0ce927fd20d9b7d0ee2db67d7f16e70017
kiwon2 <45128285+kiwon2@users.noreply.github.com>
2021-12-28 09:58:35 +0900

    [유기원] jenkins 깃허브 테스트

---
+++ jenkins/src/test/java/com/gdu/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdu.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
